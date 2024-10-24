cd5bca02367d060b340e80803c037902d0f7de52
Lerry <lvdachao@gmail.com>
2024-10-24 19:22:30 +0800

    update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ const IndexPage: React.FC = () => {
   const range = [6500, 7500] as [number, number];
 
   return (
-    <div className="w-max-5xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4 text-center">期权 PNL 图</h1>
+    <div className="w-max-5xl mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-8 text-center">ETH-7000-241201-C</h1>
       <OptionPNLChart
         strikePrice={7000}
         optionType="call"
